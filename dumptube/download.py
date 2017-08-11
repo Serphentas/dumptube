b63daea49f74063f598705f6c4963dde06c9fb76
Serphentas <Serphentas@users.noreply.github.com>
2017-08-12 01:03:01 +0200

    initial working prototype

--- dumptube/download.py
+++ dumptube/download.py
@@ -23,50 +23,32 @@ for target in targets:
         channel = channels[0]
         info("Processing channel '" + channel.username + "'")
 
-        print channel
         videos = get_videos(channel.id)
-        
+        dump_folder = os.getcwd() + '/dumps/' + target
+        chk_path(dump_folder)
+
+        # saving videos
         for video in videos:
-            print video
-        print videos[len(videos)-1].title
-        chk_path('/dumps/' + channel.username)
-        
+            info("Processing video '" + video.title + "'")
+            yt = YouTube("https://www.youtube.com/watch?v=" + video.id)
+            vid_ext = yt.videos[len(yt.videos)-1].extension
+            vid_res = yt.videos[len(yt.videos)-1].resolution
 
+            # checking if video file already exists
+            if not os.path.isfile(dump_folder + "/" + video.title + "-" + video.id + '.' + vid_ext):
+                sys.stdout.write(bcolors.OKBLUE + "Video not downloaded yet, please wait... ")
+                sys.stdout.flush()
 
-        # saving XML video feed locally
-        xml_filename = 'feed_' + channel.username + '-' + str(datetime.date.today()) + '.xml'
-        save_feed(channel.username, xml_filename)
-        root = get_feed(xml_filename)
+                dl = yt.get(vid_ext, vid_res)
+                dl.download(dump_folder)
 
-        # iterating through feed file
-        for child in root:
-            # if the current element is a video entry
-            if "entry" in child.tag:
-                # creating Video and YouTube objects
-                video = Video(child[3].text, child[1].text, child[8][3].text, child[7].text)
-                yt = YouTube("https://www.youtube.com/watch?v=" + video.id)
-
-                # printing video information    
-                print(bcolors.OKGREEN + "Entry found !" + bcolors.ENDC)
-                print("Title: " + video.title)
-                print("Date: " + video.date)
-                print("Description: " + video.description)
-                print("URL: https://www.youtube.com/watch?v=" + video.id)
-
-                # checking if video file already exists
-                if not os.path.isfile(video.title + "-" + video.id + '.mp4'):
-                    sys.stdout.write(bcolors.OKBLUE + "Video not downloaded yet, processing... ")
-                    sys.stdout.flush()
-                    dl = yt.get('mp4', '720p')
-                    dl.download('./dumps/' + channel)
-
-                    # renaming video file to original title and appending YouTube id
-                    os.rename(dl.filename + ".mp4", video.title + "-" + video.id + ".mp4")
-                    print(bcolors.OKGREEN + "done" + bcolors.ENDC)
-                    print("")
-                else:
-                    print(bcolors.OKBLUE + "Video already downloaded, skipping" + bcolors.ENDC)
-            print("")
+                # renaming video file to original title and appending YouTube id
+                os.rename(dump_folder + '/' + dl.filename + '.' + vid_ext,dump_folder + '/' + video.title + "-" + video.id + '.' + vid_ext)
+                print(bcolors.OKGREEN + "done" + bcolors.ENDC)
+                print("")
+            else:
+                print(bcolors.OKBLUE + "Video already downloaded, skipping" + bcolors.ENDC)
+                print("")
     else:
         print(bcolors.WARNING + "More than one channel has been found with the keyword '" + channel + "'" + bcolors.ENDC)
         print(bcolors.WARNING + "Please only enter exact channel names" + bcolors.ENDC)
